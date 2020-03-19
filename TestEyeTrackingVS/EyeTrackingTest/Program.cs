3fe707ce0165d87b949bfcd461da6920d84ef3c4
michael-edgar <michael.edgar@students.ittralee.ie>
2020-03-19 10:29:32 +0000

    Finished Sprint 3

---
+++ TestEyeTrackingVS/EyeTrackingTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Tobii.Interaction;
+
+namespace EyeTrackingTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
